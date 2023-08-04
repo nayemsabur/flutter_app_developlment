482d92ae1edb0a0813c8ed5cb1475f562c0deb42
nayemsabur <98073930+nayemsabur@users.noreply.github.com>
2023-08-04 16:38:07 +0600

    first commint

---
+++ bin/first_assignment.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 10;
+  int b = 3;
+  int additionResult = a + b;
+  int subtractionResult = a - b;
+  int multiplicationResult = a * b;
+  double divisionResult = a / b;
+  int modulusResult = a % b;
+  print("Addition of a and b: $additionResult");
+  print("Subtraction of b from a: $subtractionResult");
+  print("Multiplication of a and b: $multiplicationResult");
+  print("Division of a by b: $divisionResult");
+  print("Modulus of a by b: $modulusResult");
+}
